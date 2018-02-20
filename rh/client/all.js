f2783957b5b5c38a708d40a7911158a5b4f531ad
Michael Trythall <m@mtrythall.com>
2018-02-20 09:52:31 -0500

    Moves Foundation to lower package.json

--- rh/client/all.js
+++ rh/client/all.js
@@ -1,6 +1,7 @@
 import './scss/core.scss';
 
-// import * as Foundation from 'foundation-sites';
+// import * as $ from 'jquery';
+import * as Foundation from 'foundation-sites';
 import './js/app';
 
 // initialize standalone plugins/widgets
