20bed9883c25a77214ef0e977e558337b830b800
Chris Beaven <smileychris@gmail.com>
2018-04-05 16:52:59 +1200

    Add a pagination plugin
    Refs #41

--- rh/apps/meta/models.py
+++ rh/apps/meta/models.py
@@ -1,5 +1,9 @@
 from cms.extensions import PageExtension
 from cms.extensions.extension_pool import extension_pool
+from cms.models import CMSPlugin
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.db import models
 
 from filer.fields.image import FilerImageField
 from djangocms_text_ckeditor.fields import HTMLField
@@ -24,4 +28,43 @@ class MetaAttributes(PageExtension, IconMixin):
         verbose_name_plural = 'Meta Attributes'
 
 
+class Pagination(CMSPlugin):
+    show_numbers = models.BooleanField(default=True)
+
+
+# ------------------------------------------------------------------------------
+# CMS Plugin/Admin
+# ------------------------------------------------------------------------------
+
+class PaginationPlugin(CMSPluginBase):
+    name = 'Pagination'
+    module = 'Content'
+    model = Pagination
+    render_template = "cms_plugins/meta/pagination.html"
+
+    fieldsets = (
+        ('Content', {
+            'fields': ('show_numbers',)
+        }),
+    )
+
+    def render(self, context, instance, placeholder):
+        page = instance.page
+        prev_num, next_num = None, None
+        for i, sibling in enumerate(page.get_siblings()):
+            if sibling == page:
+                prev_num = i
+                next_num = i + 2
+                break
+        context.update({
+            'prev': page.get_prev_sibling(),
+            'next': page.get_next_sibling(),
+            'prev_num': prev_num,
+            'next_num': next_num,
+            'instance': instance,
+        })
+        return context
+
+
 extension_pool.register(MetaAttributes)
+plugin_pool.register_plugin(PaginationPlugin)
