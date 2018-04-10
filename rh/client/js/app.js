42159c287fdc86cd77c7be65272e198bec537438
sodevious <sodevious.net@gmail.com>
2018-04-10 19:34:07 -0400

    First pass at mobile accordion for case study filters
    #29

--- rh/client/js/app.js
+++ rh/client/js/app.js
@@ -1,6 +1,20 @@
 // initialize standalone plugins/widgets
 import $ from  './import-jquery';
 
-import * as Foundation from 'foundation-sites';
+import 'foundation-sites';
 
-$(document).foundation();
+$(document).ready(() => {
+  $(document).foundation();
+
+  if ($(".case-study-search-filters").length ){
+    if (Foundation.MediaQuery.atLeast('medium')) {
+      $('.case-study-search-filters').foundation('down', $('.case-study-search-list'), false);
+    }
+
+    $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+      if (newSize !== oldSize) {
+        $('.case-study-search-filters').foundation('down', $('.case-study-search-list'), true);
+      }
+    });
+  }
+});
